6714ec3e006135e70658c9b46570f1174baff7ec
Lazar, Viktor <viktor.lazar@sap.com>
2019-06-14 10:40:03 +0200

    Settings menu and Activity added - now it is empty

--- app/src/main/java/com/example/i060663/githubrepositories/GitHubSearchActivity.java
+++ app/src/main/java/com/example/i060663/githubrepositories/GitHubSearchActivity.java
@@ -11,6 +11,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -169,6 +171,23 @@ public class GitHubSearchActivity extends AppCompatActivity implements LoaderMan
     public void onLoaderReset(Loader<List<Repository>> loader) {
         // Loader reset, so we can clear out our existing data.
         repositoryAdapter.clear();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
